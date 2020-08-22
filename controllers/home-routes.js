f5cbc82be9d5cb6b4b524dc976865a7d2d534055
geocode-matt <mlindenx@gmail.com>
2020-08-22 13:49:14 -0500

    got basic homepage HTML and CSS set up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,44 @@ const express = require('express');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
-router.get('/', function(req, res, next) {
-    console.log('working');
-});
+const books = [
+  {
+    title: 'Love You Forever',
+    read: false,
+    author: 'Robert Munsch'
+  },
+  {
+    title: 'The Giving Tree',
+    read: false,
+    author: 'Shel Silverstein'
+  },
+  {
+    title: 'Where the Red Fern Grows',
+    read: true,
+    author: 'Wilson Rawls'
+  },
+  {
+    title: 'The Fault in Our Stars',
+    read: true,
+    author: 'John Green'
+  },
+  {
+    title: 'Out of My Mind',
+    read: false,
+    author: 'Sally Engelfried'
+  },
+  {
+    title: 'Wonder',
+    read: false,
+    author: 'Barbara Schultz'
+  }
+];
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    const data = { books: books };
+    res.render('homepage', data);
+  });
 
 module.exports = router;
